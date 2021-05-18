ced3ae318cf176e0528b7c9a30d242f365c97633
rocmoc <80848273+rocmoc@users.noreply.github.com>
2021-05-17 17:51:26 -1200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// Modificarea header'ului
+window.addEventListener("scroll", function(){
+	var header = document.querySelector("header");
+	header.classList.toggle("sticky", window.scrollY > 0);
+});
+
